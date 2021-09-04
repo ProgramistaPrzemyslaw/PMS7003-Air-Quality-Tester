ff6d7ef0042532db02240e07a4e2c03ab3db046e
Przemysław Żerański <p.zeranski@stud.elka.pw.edu.pl>
2021-09-04 14:30:05 +0200

    Added necessary libraries

---
+++ src/header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Adafruit_AHTX0.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Wire.h>
+#include <PMS.h>
