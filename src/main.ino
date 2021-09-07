d87d78f8183afd327c8b7cf9d961aa993fb76fef
Przemysław Żerański <p.zeranski@stud.elka.pw.edu.pl>
2021-09-07 10:47:31 +0200

    Displaying PM data works

--- src/main.ino
+++ src/main.ino
@@ -4,25 +4,25 @@ const char *ssid = "Air Quality Tester";
 const char *password = "123456789";  
 
 Adafruit_AHTX0 aht;
-//PMS pms(Serial);
-//PMS::DATA data;
+PMS pms(Serial);
+PMS::DATA data;
 
 
 void setup(){
-    Serial.begin(115200);
-    Serial.println("Begin");
+    Serial.begin(9600);
+    //Serial.println("Begin");
     
     if(! aht.begin()){
-        Serial.println("AHT not found");
+      //  Serial.println("AHT not found");
         while(1) delay(10);
     }
-    Serial.println("AHT found");
+    //Serial.println("AHT found");
 
     WiFi.softAP(ssid,password);
 
     IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
+    //Serial.print("AP IP address: ");
+    //Serial.println(IP);
 
     server.on("/", handleRoot);
     server.begin();
@@ -34,9 +34,13 @@ server.handleClient();
 aht.getEvent(&hum, &temp);
 Humidity = hum.relative_humidity;
 Temperature = temp.temperature;
-
-Serial.println(Humidity);
-Serial.println(Temperature);
-Serial.println("__________ ");
+if(pms.read(data)){
+PM10 = data.PM_AE_UG_10_0;
+PM25 = data.PM_AE_UG_2_5;
+PM1 = data.PM_AE_UG_1_0;
+}
+//Serial.println(Humidity);
+//Serial.println(Temperature);
+//Serial.println("__________ ");
 delay(100);
 }
