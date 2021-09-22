c54b97e218f40e2474f6bd1ab5d08f675ee6d4c6
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2021-09-22 18:15:57 +0200

    Added constructor and addHeader method

--- src/graphs.cpp
+++ src/graphs.cpp
@@ -1,3 +1,12 @@
 #include "graphs.h"
+#include "header.h"
 
 
+PM10Graph::PM10Graph(int width, int heigth,int rows, int columns):
+width_(width), heigth_(heigth),rows_(rows),colums_(columns){
+graph.reserve(10000);
+}
+
+void PM10Graph::addHeader(){
+    sprintf(graph,"<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%d\" height=\"%d\">\n",width_,heigth_);
+}
