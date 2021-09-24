2f0e0d2e1a46ae8c562029ad79cd90f80855ff13
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2021-09-24 13:10:00 +0200

    Changed addAxisLines method

--- src/graphs.h
+++ src/graphs.h
@@ -5,13 +5,12 @@ class Graph
 {
 private:
 public:
-    Graph(int heigth, int width);
     virtual void drawGraph() = 0;
     virtual std::string addHeader() = 0;
-    virtual std::string addXLabel() = 0;
-    virtual std::string addYLabel() = 0;
-    virtual std::string addXTitle() = 0;
-    virtual std::string addYTitle() = 0;
+    virtual std::string addXLabel(int beginning, int end) = 0;
+    virtual std::string addYLabel(int beginning, int end) = 0;
+    virtual std::string addXTitle(std::string title) = 0;
+    virtual std::string addYTitle(std::string title) = 0;
     virtual std::string addAxisLine(int numberOfLines) = 0;
     virtual std::string addPlot() = 0;
     virtual std::string addRectangle(int posx, int posy) = 0;
@@ -24,17 +23,20 @@ class PM10Graph: public Graph{
     int heigth_;
     int rows_;
     int colums_;
-    int margin_;
-    String graph;
+    int x_margin_;
+    int y_margin_;
+    int pos_x_;
+    int pos_y_;
+    std::string graph_;
     public:
     PM10Graph(int width, int heigth,int rows, int columns);
      void drawGraph();
      std::string addHeader();
-     std::string addXLabel();
-     std::string addYLabel();
-     std::string addXTitle();
-     std::string addYTitle();
-     std::string addAxisLine(int numberOfLines);
+     std::string addXLabel(int beginning, int end);
+     std::string addYLabel(int beginning, int end);
+     std::string addXTitle(std::string title);
+     std::string addYTitle(std::string title);
+     std::string addAxisLines(std::string axis);
      std::string addPlot();
      std::string addRectangle(int posx, int posy);
     ~PM10Graph();
